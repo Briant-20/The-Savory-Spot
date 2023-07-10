e3f47a41550b7401ec15f1763f0087de582d96f7
Brian Teahan <Brian.teahan@mycit.ie>
2023-07-10 14:43:43 +0100

    Add index.html file as default view

--- restaurantwebsite/views.py
+++ restaurantwebsite/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Body
 
-# Create your views here.
+
+class Body(generic.ListView):
+    model = Body
+    template_name = "index.html"

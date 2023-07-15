f6de8c140e28b201d599a33ed5989724a83e99ea
Brian Teahan <Brian.teahan@mycit.ie>
2023-07-15 21:08:25 +0100

    Add button titles that display items when clicked

---
+++ static/js/script.js
@@ -0,0 +1,23 @@
+let categoryTitles = document.getElementsByClassName("category-titles");
+let categoryItems = document.getElementsByClassName("category-items");
+
+let display_category = (categoryTitle) =>{
+    return function(){
+    categoryName = categoryTitle.getAttribute("name");
+    category = categoryTitle.getAttribute("category");
+    for (let i = 0; i < categoryItems.length; i++) {
+        let categoryItem = categoryItems[i];
+        itemCategory = categoryItem.getAttribute("category");
+        if (itemCategory === category){
+            categoryItem.style.display = 'flex';
+        }
+        else{
+            categoryItem.style.display = 'none';
+        }
+    }
+    }
+}
+for (let i = 0; i < categoryTitles.length; i++) {
+    let categoryTitle = categoryTitles[i];
+    categoryTitle.addEventListener("click", display_category(categoryTitle));
+}

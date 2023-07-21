750422964c448454f8c90998b9c52a27f6316ece
Brian Teahan <Brian.teahan@mycit.ie>
2023-07-21 17:56:34 +0100

    Add a day selector depending on the month

--- static/js/script.js
+++ static/js/script.js
@@ -21,3 +21,30 @@ for (let i = 0; i < categoryTitles.length; i++) {
     let categoryTitle = categoryTitles[i];
     categoryTitle.addEventListener("click", display_category(categoryTitle));
 }
+
+$(document).ready(function () {
+    const daysPerMonth = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    }
+
+        $('#month').change(function () {
+            let selectedMonth = $(this).val();
+            let days = daysPerMonth[selectedMonth];
+            let daySelector = $('#day');
+            daySelector.empty();
+            for (let day = 1; day <= days; day++) {
+                daySelector.append(`<option value="${day}">${day}</option>`);
+            }
+        });
+    });

7748be240bea3af82444a553342c9288022b540c
Brian Teahan <Brian.teahan@mycit.ie>
2023-08-03 14:28:00 +0100

    Test menu models

---
+++ menu/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from .models import Menu, Category, Item
+
+
+class MenuModelTest(TestCase):
+    def setUp(self):
+        self.menu = Menu.objects.create(title='Test Menu', content='Test Content')
+        self.category = Category.objects.create(menus=self.menu, title='Test Category')
+        self.item = Item.objects.create(categories=self.category, name='Test Item', description='Test Description', price=10.00)
+
+    def test_menu_creation(self):
+        self.assertEqual(str(self.menu), 'Test Menu')
+
+    def test_category_creation(self):
+        self.assertEqual(str(self.category), 'Test Category')
+
+    def test_item_creation(self):
+        self.assertEqual(str(self.item), 'Test Item')
